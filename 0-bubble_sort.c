59093dbc124877b0b9e207a02a522893c0a166d7
Quesses <quesses1@gmail.com>
2022-11-08 12:10:10 +0000

    0-1

---
+++ 0-bubble_sort.c
@@ -0,0 +1,33 @@
+#include "sort.h"
+
+/**
+ * void bubble_sort: implements the bubble sort algorithm on an array of integers
+ *
+ * @array: array of integers
+ * @size: the size of the given array
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j = size;
+	int tmp, flag;
+
+	if (array == NULL || size < 2)
+		return;
+	while(j > 0 && flag)
+	{
+		flag = 0;
+		
+		for(i = 0; i < (j - 1); i++)
+		{
+			if(array[i] > array[i + 1])
+			{
+				tmp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = tmp;
+				print_array(array, size);
+				flag = 1;
+			}
+		}
+		j -= 1;
+	}
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+Sorting Algorithms in C programming
+
+Learning several new sorting algorithms:-
+Bubble sort
+Simple sort
+Insertion sort
+Selection sort and several more
